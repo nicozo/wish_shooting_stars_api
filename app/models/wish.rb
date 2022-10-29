53623109a3e6fc2937b69864ca0a06163ccf025a
nicozo <niczo.soccer161@gmail.com>
2022-10-29 16:29:41 +0900

    Update: 願い事語尾のバリデーションを追加

--- app/models/wish.rb
+++ app/models/wish.rb
@@ -1,5 +1,6 @@
 class Wish < ApplicationRecord
-  validates :title, presence: true, length: { maximum: 15 }
+  VALID_WISH_REGEX = /欲しい\z|ほしい\z|下さい\z|ください\z|ように\z/
+  validates :title, presence: true, length: { maximum: 15 }, format: { with: VALID_WISH_REGEX }
 
   scope :successful_wishes, -> { where(success: true).order(created_at: :desc) }
 end
