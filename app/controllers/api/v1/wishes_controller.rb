fd7ffa1b7d683a0ab2631d41ec489d7bb6b603d4
nicozo <niczo.soccer161@gmail.com>
2022-11-23 13:21:53 +0900

    Add: updateアクションを追加

--- app/controllers/api/v1/wishes_controller.rb
+++ app/controllers/api/v1/wishes_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::WishesController < ApplicationController
+  before_action :set_wish, only: %i[update]
+
   def index
     wishes = Wish.successful_wishes
 
@@ -15,9 +17,25 @@ class Api::V1::WishesController < ApplicationController
     end
   end
 
+  def update
+    if @wish.update(score_params)
+      render json: @wish
+    else
+      render json: @wish.errors, status: :bad_request
+    end
+  end
+
   private
 
   def wish_params
     params.require(:wish).permit(:title)
+  end
+
+  def score_params
+    params.require(:wish).permit(:success, :score)
+  end
+
+  def set_wish
+    @wish = Wish.find(params[:id])
   end
 end
