3c9a38c754fb638f88b857750824bdc46497892e
nicozo <niczo.soccer161@gmail.com>
2022-12-01 13:00:13 +0900

    Edit: コメントアウトを削除

--- app/controllers/api/v1/wishes_controller.rb
+++ app/controllers/api/v1/wishes_controller.rb
@@ -2,7 +2,6 @@ class Api::V1::WishesController < ApplicationController
   before_action :set_wish, only: %i[update]
 
   def index
-    # wishes = Wish.successful_wishes(100)
     wishes = Wish.all.order(created_at: :desc)
 
     render json: wishes
