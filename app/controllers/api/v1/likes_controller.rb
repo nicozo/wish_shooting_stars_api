0309dd283766e6322fd55458d02247c7ce2b5ff7
nicozo <niczo.soccer161@gmail.com>
2022-12-01 18:28:59 +0900

    Add: likes_controller.rbを作成

---
+++ app/controllers/api/v1/likes_controller.rb
@@ -0,0 +1,17 @@
+class Api::V1::LikesController < ApplicationController
+  def create
+    like = Like.new(likes_params)
+
+    if like.save
+      render json: like
+    else
+      render json: like.errors, status: :bad_request
+    end
+  end
+
+  private
+
+  def likes_params
+    params.require(:like)permit(:wish_id)
+  end
+end
--- app/models/wish.rb
+++ app/models/wish.rb
@@ -1,5 +1,7 @@
 class Wish < ApplicationRecord
   VALID_WISH_REGEX = /欲しい\z|ほしい\z|下さい\z|ください\z|ように\z/
+  has_many :likes, dependent: :destroy
+
   validates :title, presence: true, length: { maximum: 15 }, format: { with: VALID_WISH_REGEX }
 
   scope :successful_wishes, -> (number) { where('score = ?', number ).order(created_at: :desc) }
