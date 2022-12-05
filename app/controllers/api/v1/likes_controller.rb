a588e290977b62ff9548a78d306d7d169706175c
nicozo <niczo.soccer161@gmail.com>
2022-12-05 19:19:18 +0900

    Update: ログイン不要でいいね機能を実装

--- app/controllers/api/v1/likes_controller.rb
+++ app/controllers/api/v1/likes_controller.rb
@@ -1,11 +1,19 @@
 class Api::V1::LikesController < ApplicationController
   def create
-    like = Like.new(likes_params)
+    wish = Wish.find(params[:like][:wish_id])
+    already_like = Like.find_by(ip: request.remote_ip, wish_id: wish.id)
 
-    if like.save
-      render json: like
+    if already_like
+      render json: already_like, status: :bad_request
     else
-      render json: like.errors, status: :bad_request
+      like = Like.new(likes_params)
+      like.ip = request.remote_ip
+
+      if like.save
+        render json: like
+      else
+        render json: like.errors, status: :bad_request
+      end
     end
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_01_041909) do
+ActiveRecord::Schema.define(version: 2022_12_05_100113) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2022_12_01_041909) do
     t.bigint "wish_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "ip"
     t.index ["wish_id"], name: "index_likes_on_wish_id"
   end
 
